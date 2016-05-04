bbee1b6b287cfc789013342f89dcd175993d6b7a
Brett Weinkauf <bweinkau@ualberta.ca>
2016-05-04 19:22:51 +0000

    added in some general javascript for search capabilities

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,27 @@
-$(document).ready(function() {
+$(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  function receiveContacts(contacts) {
+    $.each(contacts);
+  }
+
+  function getContacts() {
+    $("#displayContacts").empty();
+    $.getJSON("/contacts", receiveContacts);
+  }
+
+  getContacts();
+
+  $("#search-field").on("keyup", function() {
+    var searchInput = $(this).val().toLowerCase();
+    $("#displayContacts tr").each(function() {
+      var searchOutput = $(this).text().toLowerCase();
+      if (searchOutput.indexOf(searchInput)!=-1) {
+        $(this).show();
+      }
+      else {
+        $(this).hide();
+      }
+    });
+  });
+
 });
