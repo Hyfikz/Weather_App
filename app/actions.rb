bcf36a05b979eb49fd028b2cff741d7029609300
Brett Weinkauf <bweinkau@ualberta.ca>
2016-05-04 19:12:47 +0000

    Starter layout and actions

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,23 @@ end
 get '/weathers' do
   Weather.all.to_json
 end
+
+post '/cities' do
+  results = {result: false}
+  name = params[:name]
+  current_temp = params[:current_temp]
+  tomorrow_temp = params[:tomorrow_temp]
+  city = City.new name: name, current_temp: current_temp, tomorrow_temp: tomorrow_temp
+  p city
+  if city.save
+    results[:result] = true
+    results[:city] = {id: city.id}
+  end
+  results.to_json
+end
+
+#show city details
+get '/cities/:id' do
+  city = City.find params[:id]
+  city.to_json
+end
